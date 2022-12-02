811bd045074a38fee439ab0d6228a5beab53c4d3
jsniemela <niemelajonne@gmail.com>
2022-12-02 20:47:11 +0200

    Created basic UI

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalculatorWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void button_Copy_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_Copy1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_Copy2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_Copy5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_Copy12_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_Copy13_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
